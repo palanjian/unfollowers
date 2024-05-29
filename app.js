ef8605d9f959f86f6f55a65aee9e4ad2ff696cd1
palanjian <ppalanji@usc.edu>
2024-05-29 00:00:37 -0700

    Added file upload

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const multer = require('multer')
 const ejs = require('ejs')
+const path = require('path');
 
 const app = express()
 const upload = multer({dest: 'uploads/'})
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
 //users multer middleware, before we run our logic multer runs its
 //file == name on html form
 app.post('/api/upload', upload.single('file'), (req, res) => {  
-    res.send(req.file)
+    res.render('index', { fileinfo: `Name of file: ${req.file.filename}` })
 })
 
-
 app.get('/', (req, res) => {
-    res.render(index)
+    res.render('index', { fileinfo: null})
 })
 
 const port = process.env.PORT || 3000
