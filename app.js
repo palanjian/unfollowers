51a04bed5138f216755a9c959b30381ac11e7266
palanjian <ppalanji@usc.edu>
2024-05-29 23:55:59 -0700

    Displays all followers w href to profiles

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const ejs = require('ejs')
 const path = require('path');
 
 const app = express()
-const upload = multer({dest: 'uploads/'})
+const upload = multer({ storage: multer.memoryStorage() })
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,11 +13,23 @@ app.set('view engine', 'ejs');
 //users multer middleware, before we run our logic multer runs its
 //file == name on html form
 app.post('/api/upload', upload.single('file'), (req, res) => {  
-    res.render('index', { fileinfo: `Name of file: ${req.file.filename}` })
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
+
+    if (req.file.mimetype !== 'application/json') {
+        return res.status(400).send('Invalid file type. Only JSON files are allowed.');
+    }
+
+    const jsonData = JSON.parse(req.file.buffer.toString());
+    res.render('index', { people: jsonData });
 })
 
 app.get('/', (req, res) => {
-    res.render('index', { fileinfo: null})
+
+    res.render('index', {
+        people: []
+    })
 })
 
 const port = process.env.PORT || 3000
